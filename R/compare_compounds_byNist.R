da3478d82b7184f95db6f48c93f9f0b2fcf81684
Shun Hasegawa <snwa0003@umea-88-e9-fe-66-b1-7b.wifi.slu.se>
2020-09-30 11:37:59 +0200

    aligning compounds from NIST to compare results from multiple sites

---
+++ R/compare_compounds_byNist.R
@@ -0,0 +1,49 @@
+
+# Here, I align the lists of compounds by retention time identified by NIST from
+# multiple sampling sites to check if assigned compounds are reasonalble relative
+# to the other sites
+
+source("R/packages.R")
+
+comp_fls <- dir("Data/NIST_compound_comparison/")
+
+
+site <- strsplit(comp_fls[1], "_|[.]")[[1]][3]
+type <- strsplit(comp_fls[1], "_|[.]")[[1]][2]
+
+d1 <- read.csv(paste0("Data/NIST_compound_comparison/", comp_fls[1])) %>% 
+  select(Window, RT_s, Comp)
+colnames(d1) <- c(paste("Window", site, type, sep = "_"),
+                  "RT_s",
+                  paste("Comp", site, type, sep = "_"))
+
+
+all_comps <- llply(comp_fls, function(x){
+  site <- strsplit(x, "_|[.]")[[1]][3]
+  type <- strsplit(x, "_|[.]")[[1]][2]
+  d <- read.csv(paste0("Data/NIST_compound_comparison/", x)) %>% 
+    mutate(RT_s = round(RT_s, 0)) %>%  # round RT_s
+    select(Window, RT_s, Comp) %>% 
+    group_by(RT_s) %>%
+    mutate(rep = 1:n())                # Some RT_s have multiple values so add rep number for each 
+  colnames(d) <- c(paste(type, site, "Window", sep = "_"),
+                    "RT_s",
+                    paste(type, site, "Comp", sep = "_"),
+                   rep)
+  return(d)
+  
+})
+
+
+merged_comp <- Reduce(function(...) merge(..., all = TRUE), all_comps) %>% 
+  select(-contains("rep")) %>% 
+  mutate_all(.funs = funs(ifelse(is.na(.), "", as.character(.))))
+
+# check if there is no redundant rows
+sapply(all_comps, nrow)
+merged_comp %>% 
+  select(contains("Window")) %>% 
+  apply(., 2, function(x) sum(!is.na(x)))
+
+# save
+write.csv(merged_comp, "Output/Tables/Compare_NIST_addigned_compds.csv", row.names = FALSE)
